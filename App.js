f80b8fcb357238e53f3b74a47402e9f04451b676
Fossdahl <41930462+Fossdahl@users.noreply.github.com>
2018-08-16 20:27:51 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className = "App-background">
+        <header className="App-header">
+          <h1 className="App-title">Justin er til Lil Pump</h1>
+        </header>
+          {/* <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+          </p> */}
+          <p1>
+          <img src="https://cdn.spinrilla.com/albums/98009/large/194f7760df028e450601.jpg?1518852325" alt="Justins lover" className="App-logo"/>
+          </p1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
